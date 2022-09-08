5f5420441cd18590f63270d696bb8ee6a3f14923
Fresh <freshanoyntyn@gmail.com>
2022-09-08 11:40:34 -0700

    Solution to c task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -13,8 +13,8 @@ int main()
 
 {
 
-		puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
-			return (0);
+	return (0);
 
 }
