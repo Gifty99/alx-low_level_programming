b3a3a4f05f47e68ad5f0de491fd307a704da47ef
Fresh <freshanoyntyn@gmail.com>
2022-09-08 09:43:22 -0700

    Solution to task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,20 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+ *
+ *  * main function that does the following;
+ *   * Print
+ *   * Return: 0
+ *
+ *    */
+
+int main()
+
+{
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+
+}
