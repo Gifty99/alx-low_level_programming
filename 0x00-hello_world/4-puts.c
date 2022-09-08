a72048c7de8a665cf0b17e058cd1e74c3eab4b19
Fresh <freshanoyntyn@gmail.com>
2022-09-08 12:11:16 -0700

    Solution to c task 4____

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,14 +2,13 @@
 
 /*
  *
- *  * main function that does the following;
+ *  * main function that contains the print function
  *
  *   * Return:
  *
- *    */
+ */
 
 int main(void)
-
 {
 
 	puts("\"Programming is like building a multilingual puzzle");
