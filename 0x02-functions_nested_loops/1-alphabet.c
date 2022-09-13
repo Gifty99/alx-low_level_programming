c102bd6d9af6832f4e9ede33c751574e9c3b72f5
Fresh <freshanoyntyn@gmail.com>
2022-09-13 14:36:18 -0700

    Solution to task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * main - main block
+ *
+ * Description: Print the alphabet in lowercase followed by a new line
+ *
+ * Return: 0
+ *
+ */
+
+
+void print_alphabet(void)
+
+{
+
+	char alphabet;
+
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+
+		_putchar(alphabet);
+
+	_putchar('\n');
+
+}
