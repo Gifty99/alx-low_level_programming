7c4ce4cef5a359217b888df470cecee3e542627e
Fresh <freshanoyntyn@gmail.com>
2022-09-13 18:32:39 -0700

    Solution to task 9____

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,31 +12,24 @@ void times_table(void)
 {
 
 	int x; /* factor */
-
 	int y; /* count  */
-
 	int z; /* computed value */
-
 
 	for (x = 0; x < 10; x++)
 	{
-
 		for (y = 0; y < 10; y++)
 		{
-
 			z = x * y;
 
 			if (z > 9)
 			{
 				_putchar((z / 10) + '0');
-
 				_putchar((z % 10) + '0');
 			}
 
 			else if (y != 0)
 			{
 				_putchar(' ');
-
 				_putchar(z + '0');
 			}
 
@@ -49,15 +42,12 @@ void times_table(void)
 			if (y != 9)
 			{
 				_putchar(',');
-
 				_putchar(' ');
 			}
-
 
 		}
 
 		_putchar('\n');
-
 	}
 
 }
