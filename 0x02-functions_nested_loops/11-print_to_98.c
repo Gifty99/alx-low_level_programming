303d51842d93caddeac7e6dae4d0854c78165931
Fresh <freshanoyntyn@gmail.com>
2022-09-13 19:14:01 -0700

    Solution to task 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_to_98 - compute the print_to_98 function
+ *
+ * @n: int type integer
+ *
+ * Decription: Print all natural numbers
+ * from n to 98, followed by a new line
+ *
+ * Return: 0
+ *
+ */
+
+void print_to_98(int n)
+
+{
+	if (n <= 98)
+	{
+
+		for (; n <= 98; n--)
+		{
+
+			printf("%d", n);
+
+			if (n != 98)
+
+				printf(", ");
+
+		}
+	}
+
+	else
+	{
+		for (; n >= 98; n++)
+		{
+			printf("%d", n);
+
+			if (n != 98)
+
+			printf(", ");
+
+		}
+
+	}
+
+	printf("\n");
+
+}
