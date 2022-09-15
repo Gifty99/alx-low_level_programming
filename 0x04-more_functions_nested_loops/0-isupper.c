26b9cab57894e48e1969597aef4088f5b66dfd70
Fresh <freshanoyntyn@gmail.com>
2022-09-15 13:52:46 -0700

    Solution to task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -25,16 +25,14 @@ int _isupper(int c)
 
 	return (i);
 
+	/**
+	* {
+	*	if (c >= 65 && c <= 90)
+	*
+	*		return (1);
+	*
+	*	else
+	*		return (0);
+	* }
+	*/
 }
-
-/**
- * int _isupper(int c)
- * {
- * 	if (c >= 65 && c <= 90)
- *
- * 		return (1);
- *
- * 	else
- * 		return (0);
- * }
- */
