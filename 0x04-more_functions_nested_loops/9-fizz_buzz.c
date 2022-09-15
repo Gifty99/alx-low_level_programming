c141d0337ff6e90730744ef1e6ff24ddbe96c12a
Fresh <freshanoyntyn@gmail.com>
2022-09-15 16:30:05 -0700

    Solution to task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -7,28 +7,26 @@
 
 int main(void)
 {
-	int f;
+	int f = 1;
 
-	for (f = 1; f < 100 ; f++)
+	while (f < 100)
 	{
 		if (f % 3 == 0 && f % 5 == 0)
-
 			printf("FizzBuzz ");
 
 		else if (f % 3 == 0)
-
 			printf("Fizz ");
 
 		else if (f % 5 == 0)
-
 			printf("Buzz ");
 
 		else
-
 			printf("%d ", f);
+
+		f++;
 	}
 
-	printf("\n");
+	printf("Buzz\n");
 
 	return (0);
 }
