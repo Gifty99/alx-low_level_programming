48286da715758cc63669c1a6f1624b3e1ba07a7c
Fresh <freshanoyntyn@gmail.com>
2022-09-15 18:18:09 -0700

    I need to sleep

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,24 +7,21 @@
 
 void print_number(int n)
 {
-	unsigned int n1;
+	unsigned int x;
 
+	x = n;
 
 	if (n < 0)
 	{
-		n1 = -n;
+		_putchar(45);
 
-		_putchar('-');
+		x = -n;
 	}
 
-	else
-		n1 = n;
+	if (x / 10)
 
-	if (n1 / 10)
-	{
-		print_number(n1 / 10);
-	}
+		print_number(x / 10);
 
-	_putchar((n1 % 10) + '0');
+	_putchar((x % 10) + '0');
 
 }
