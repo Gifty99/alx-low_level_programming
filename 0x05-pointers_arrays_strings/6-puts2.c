cf32063d0e4e43d4dac6e82c9de36a98d581ed12
Fresh <freshanoyntyn@gmail.com>
2022-09-19 15:58:35 -0700

    Solution to task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * puts2 - a function that prints every character of a string
+ * starting with the first character, followed by a new line.
+ * @str: input string
+ *
+ * Return: nothing
+ */
+
+void puts2(char *str)
+{
+	int length, m;
+
+	for (str[length] != '\0'; length++)
+
+	length -= 1;
+
+	for (m = 0; m <= length; m += 2)
+		_putchar(str[m]);
+
+	_putchar('\n');
+}
