9913ec9f153d4ae3c1d59f410377dc4743b1ebf5
Fresh <freshanoyntyn@gmail.com>
2022-09-21 07:40:24 -0700

    Solution to task 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ *cap_string - A function that capitalizes all words of a string
+ *@s: input string
+ *
+ *Return: pointer to converted string
+ */
+
+char *cap_string(char *s)
+{
+	int x = 0, y;
+	char separators[] = {',', ';', '.', '!', '?', '"', '(', ')', '{', '}' }
+
+	while (*(s + x))
+	{
+		for (y = 0; y < 11; y++)
+		{
+			if (x == 0 && s[x] >= 'a' && s[x] <= 'z')
+				s[x] -= ' ';
+
+			if (s[x - 1] == separators[y])
+			{
+				if ((*(s + x) >= 'a') &&  (*(s + x) <= 'z'))
+					*(s + x) -= ' ';
+			}
+		}
+
+		x++;
+	}
+
+	return (s);
+}
