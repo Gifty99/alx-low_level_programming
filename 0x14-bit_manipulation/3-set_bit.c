61e68bf27a868b506640ad769bf0628a6f0f7c8f
Fresh <freshanoyntyn@gmail.com>
2022-10-27 15:30:45 -0700

    101

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -9,14 +9,13 @@
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int max = 0x01;
+	unsigned long int max;
 
-	max <<= index;
-
-	if (max == 0)
+	if (index > 63)
 		return (-1);
 
-	*n |= max;
+	max = 1 << index;
+	*n = (*n | max);
 
 	return (1);
 }
